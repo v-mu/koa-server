c5673fb674f35ff0cfb53d3137baad880b46facf
慧宁师 <cc2e@qq.com>
2019-03-26 18:11:48 +0800

    first v

---
+++ config/index.js
@@ -0,0 +1,10 @@
+/*
+	app.js 主配置文件
+	auth.js 用户登录 认证配置
+	database.js  数据库的配置文件
+	filesystems.js  上传文件，存储需要用到的配置文件
+	mail.js   email配置 
+	session.js   会话控制
+	view.js     视图文件配置 
+	cache.js    缓存配置 
+*/
